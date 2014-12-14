60f76a4b354a1e60eca2a5b6ad011d099c8f8868
Arba Sasmoyo <asasmoyo@student.uns.ac.id>
2014-12-14 17:20:19 +0700

    initial commit

---
+++ simplesamlphp-sp/metadata/saml20-idp-remote.php
@@ -0,0 +1,24 @@
+<?php
+
+$metadata['http://simplesamlphp-idp-example.dev/saml2/idp/metadata.php'] = array (
+  'metadata-set' => 'saml20-idp-remote',
+  'entityid' => 'http://simplesamlphp-idp-example.dev/saml2/idp/metadata.php',
+  'SingleSignOnService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'http://simplesamlphp-idp-example.dev/saml2/idp/SSOService.php',
+    ),
+  ),
+  'SingleLogoutService' => 
+  array (
+    0 => 
+    array (
+      'Binding' => 'urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect',
+      'Location' => 'http://simplesamlphp-idp-example.dev/saml2/idp/SingleLogoutService.php',
+    ),
+  ),
+  'certData' => 'MIIEFTCCAv2gAwIBAgIJALGDj9Qd7TNEMA0GCSqGSIb3DQEBCwUAMIGgMQswCQYDVQQGEwJJRDEUMBIGA1UECAwLSmF3YSBUZW5nYWgxEjAQBgNVBAcMCVN1cmFrYXJ0YTEMMAoGA1UECgwDVU5TMRcwFQYDVQQLDA5VUFQgUHVza29tIFVOUzEVMBMGA1UEAwwMQXJiYSBTYXNtb3lvMSkwJwYJKoZIhvcNAQkBFhphc2FzbW95b0BzdHVkZW50LnVucy5hYy5pZDAeFw0xNDEyMTQwNDUyNTNaFw0yNDEyMTMwNDUyNTNaMIGgMQswCQYDVQQGEwJJRDEUMBIGA1UECAwLSmF3YSBUZW5nYWgxEjAQBgNVBAcMCVN1cmFrYXJ0YTEMMAoGA1UECgwDVU5TMRcwFQYDVQQLDA5VUFQgUHVza29tIFVOUzEVMBMGA1UEAwwMQXJiYSBTYXNtb3lvMSkwJwYJKoZIhvcNAQkBFhphc2FzbW95b0BzdHVkZW50LnVucy5hYy5pZDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAOmygG7Zev1UXF/ft4Mjq/EbUlLXnfr91KIzHhT9rUARyL/ClzyS7SRHOw83fghL8lIOz6WpQuclHiOIpy+vroxhEGr/3Jzx9xtDgNnoWLwoKE4aGY94vQAqhorg9s8mW4YaSijbUPCCz+BUnYB7M/WAq/1h7E5EAhIlWMqdyy6CMW7s7/A8DLJw7pCXhsYcZFHwTbfZhYtXGNG0Zmm8BbgeDWxBitUVBxeOpeyOginxucnUBvPxsc67Y/bQFR3RTyDjza/mZ+e8Gwp05hcmwGYmbxt0TLue63qGDltXDanb7lUMQyjl4JK6Fl83UYzP7M1cCwoDnmqKfXlOUCZuLUsCAwEAAaNQME4wHQYDVR0OBBYEFJpsETZc5oxb0yi7AhaFwqgoMvpOMB8GA1UdIwQYMBaAFJpsETZc5oxb0yi7AhaFwqgoMvpOMAwGA1UdEwQFMAMBAf8wDQYJKoZIhvcNAQELBQADggEBAHo1jCE0FPhoKTRt36CJc0oNS0OeA01SBrWSM40W+YFem+WInZjqdjV1hI3HQIIWuIlkBQ8hP4fNxdHgoLFq+ihF00mMLznxScAvWdtN97d8QDTCjViNrL9H/wNnCPiwKNJng8w5VhVA6Ah5LnZT9OIZqG9A3qoM3/a8hpwqfS2YqjR62K/qKgiQQLbovdIqdfgXrRLjtZToJykJEnNdfrGTVh/O2yUQtuBurNXoEg/WQtsvM+hd7yo9+hN08Ydc0rrTTg5n9UHtQTgYmDJ6C9DKwOSGNkOaWQVDF94/f1xc5AlejpaVySP/UMCj2WgOrmSuh8UHgz1WtnbeIsPzqag=',
+  'NameIDFormat' => 'urn:oasis:names:tc:SAML:2.0:nameid-format:transient',
+);
